b8215da4f9bbebc2760fff9c76f5cb7f37b24c25
neodaoist <neo@hyperforge.xyz>
2024-04-21 15:33:33 -0400

    initial functionality

--- src/OAR.sol
+++ src/OAR.sol
@@ -1,30 +1,106 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
-contract OAR {
+import {
+    IEAS,
+    AttestationRequest,
+    AttestationRequestData,
+    RevocationRequest,
+    RevocationRequestData
+} from "eas-contracts/IEAS.sol";
+import {NO_EXPIRATION_TIME, EMPTY_UID} from "eas-contracts/Common.sol";
+import {Owned} from "solmate/auth/Owned.sol";
+import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
+
+contract OAR is Owned {
     /////////
+
+    using SafeTransferLib for address;
+
+    ///////// Events
+
+    //
+
+    ///////// Errors
+
+    error InvalidAddress();
+
+    error InsufficientEtherOrNotVerified();
+
+    ///////// Structs
+
+    struct SubmissionParams {
+        string name;
+        string description;
+        string category;
+        string url;
+        address contractAddress;
+        bytes4 functionSelector;
+        address assetAddress;
+        uint256 assetAmount;
+        uint256 maxActionsPerWallet;
+    }
+
+    ///////// State
+
+    // The address of the global EAS contract
+    IEAS private immutable eas;
+
+    // The schema ID of the OAR schema
+    bytes32 private immutable schemaId;
+
+    ///////// Construction
+
+    /// @notice Creates a new OAR instance
+    /// @param _eas The address of the global EAS contract
+    constructor(address _eas, bytes32 _schemaId) Owned(msg.sender) {
+        if (_eas == address(0)) {
+            revert InvalidAddress();
+        }
+
+        eas = IEAS(_eas);
+        schemaId = _schemaId;
+    }
 
     ///////// Submit
 
-    function submit(
-        string memory name,
-        string memory description,
-        string memory category,
-        string memory url,
-        string memory imageUrl,
-        string memory verb,
-        address contractAddress,
-        bytes4 functionSelector,
-        address assetAddress,
-        uint256 assetAmount,
-        uint256 maxActionsPerWallet
-    ) public {
-        // 
+    function submit(SubmissionParams memory params) public payable returns (bytes32 uid) {
+        // Check sufficient ether
+        // TODO add VerifiedAddress check
+        if (msg.value < 0.001 ether) {
+            revert InsufficientEtherOrNotVerified();
+        }
+
+        // Submit to registry
+        uid = eas.attest(
+            AttestationRequest({
+                schema: schemaId,
+                data: AttestationRequestData({
+                    recipient: params.contractAddress,
+                    expirationTime: NO_EXPIRATION_TIME,
+                    revocable: false,
+                    refUID: EMPTY_UID,
+                    data: abi.encode(
+                        params.name,
+                        params.description,
+                        params.category,
+                        params.url,
+                        params.contractAddress,
+                        params.functionSelector,
+                        params.assetAddress,
+                        params.assetAmount,
+                        params.maxActionsPerWallet
+                    ),
+                    value: 0
+                })
+            })
+        );
     }
 
     ///////// Sweep
 
-    function sweep() public {
-        // 
+    function sweep() public onlyOwner {
+        // Sweep funds to owner
+        owner.safeTransferETH(address(this).balance);
     }
 }
--- test/OAR.t.sol
+++ test/OAR.t.sol
@@ -1,8 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
+// Test
 import {Test, console} from "forge-std/Test.sol";
 import {MockERC721} from "./util/MockERC721.sol";
+
+// EAS
+import {
+    IEAS,
+    Attestation,
+    AttestationRequest,
+    AttestationRequestData,
+    RevocationRequest,
+    RevocationRequestData
+} from "eas-contracts/IEAS.sol";
+import {ISchemaRegistry, SchemaRecord, ISchemaResolver} from "eas-contracts/ISchemaRegistry.sol";
+import {NO_EXPIRATION_TIME, EMPTY_UID} from "eas-contracts/Common.sol";
 
 // CUT
 import {OAR} from "../src/OAR.sol";
@@ -12,74 +25,162 @@ contract OARTest is Test {
 
     OAR private oar;
 
+    ///////// State
+
+    address private contractAddress;
+    bytes32 private schemaId;
+
+    ///////// Constants
+
+    address private constant ME = address(0xCAFE);
+
+    IEAS private constant EAS = IEAS(0x4200000000000000000000000000000000000021);
+    address private constant SCHEMA_REGISTRY = 0x4200000000000000000000000000000000000020;
+
+    string private constant SCHEMA =
+        "string name, string description, string category, string url, string imageUrl, address contractAddress, bytes4 functionSelector, address assetAddress, uint256 assetAmount, uint256 maxActionsPerWallet, uint256 startDate, uint256 endDate";
+
     string private constant NAME = "My App";
     string private constant DESCRIPTION = "My App Description";
     string private constant CATEGORY = "My App Category";
     string private constant URL = "https://myapp.com";
     string private constant IMAGE_URL = "https://myapp.com/image.png";
 
-    string private constant VERB = "mint";
-    address private CONTRACT_ADDRESS;
     bytes4 private constant FUNCTION_SELECTOR = bytes4(keccak256("mint(address,uint256)"));
     address private constant ASSET_ADDRESS = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-    uint256 private constant ASSET_AMOUNT = 0.001 ether;
+    uint256 private constant ASSET_AMOUNT = 0.0022 ether;
     uint256 private constant MAX_ACTIONS_PER_WALLET = 1;
+    uint256 private constant START_DATE = 7 days;
+    uint256 private constant END_DATE = 14 days;
 
     function setUp() public {
+        vm.createSelectFork("base_sepolia");
+
+        // Background:
         // Given the OAR schema is registered with EAS
-        //
+        schemaId = ISchemaRegistry(SCHEMA_REGISTRY).register({
+            schema: SCHEMA,
+            resolver: ISchemaResolver(address(0)),
+            revocable: false
+        });
 
         // And the OAR contract is deployed
-        oar = new OAR();
+        vm.prank(ME);
+        oar = new OAR(address(EAS), schemaId);
     }
 
-    ///////// Submit
+    ///////// function submit(SubmissionParams memory params) public payable returns (bytes32 uid);
 
     function test_submit() public {
         // When I deploy an NFT contract
-        CONTRACT_ADDRESS = address(new MockERC721("Once Upon a Time in Shaolin", "WU"));
+        contractAddress = address(new MockERC721("Once Upon a Time in Shaolin", "WU"));
 
-        // And I submit an app
-        oar.submit({
-            // App
+        // And I submit it to OAR
+        bytes32 uid = oar.submit{value: 0.001 ether}(OAR.SubmissionParams({
             name: NAME,
             description: DESCRIPTION,
             category: CATEGORY,
             url: URL,
-            imageUrl: IMAGE_URL,
-
-            // Action
-            verb: VERB,
-            contractAddress: CONTRACT_ADDRESS,
+            contractAddress: contractAddress,
             functionSelector: FUNCTION_SELECTOR,
             assetAddress: ASSET_ADDRESS,
             assetAmount: ASSET_AMOUNT,
             maxActionsPerWallet: MAX_ACTIONS_PER_WALLET
-        });
+        }));
 
-        // Then I should see the app listed in OAR
-        //
+        // Then I should see my NFT contract listed in OAR
+        Attestation memory attestation = EAS.getAttestation(uid);
+
+        assertEq(attestation.uid, uid, "uid mismatch");
+        assertEq(attestation.recipient, contractAddress, "recipient mismatch");
+        assertEq(attestation.attester, address(oar), "attester mismatch");
+        assertEq(attestation.schema, schemaId, "schema mismatch");
+        // TODO assert data
     }
 
-    function test_submit_whenNotEnoughEtherButVerified() public {}
+    // function test_submit_many() public {
+    //     // TODO
+    // }
 
-    function test_submit_many() public {}
+    // function test_submit_whenInsufficientEtherButVerified() public {
+    //     // TODO
+    // }
 
     // Sad Paths
 
-    function testRevert_submit_whenNotSufficientEtherAndNotVerified() public {}
+    function testRevert_submit_whenInsufficientEther() public {
+        // Then the function should revert
+        vm.expectRevert(OAR.InsufficientEtherOrNotVerified.selector);
 
-    ///////// Sweep
+        // When I submit to OAR with insufficient ether
+        oar.submit(OAR.SubmissionParams({
+            name: NAME,
+            description: DESCRIPTION,
+            category: CATEGORY,
+            url: URL,
+            contractAddress: contractAddress,
+            functionSelector: FUNCTION_SELECTOR,
+            assetAddress: ASSET_ADDRESS,
+            assetAmount: ASSET_AMOUNT,
+            maxActionsPerWallet: MAX_ACTIONS_PER_WALLET
+        }));
+    }
+
+    ///////// function sweep() public;
 
     function test_sweep() public {
-        // When I sweep OAR
+        uint256 balance = ME.balance;
+
+        // Given there are multiple submissions
+        oar.submit{value: 0.001 ether}(OAR.SubmissionParams({
+            name: "name 1",
+            description: DESCRIPTION,
+            category: CATEGORY,
+            url: URL,
+            contractAddress: contractAddress,
+            functionSelector: FUNCTION_SELECTOR,
+            assetAddress: ASSET_ADDRESS,
+            assetAmount: ASSET_AMOUNT,
+            maxActionsPerWallet: MAX_ACTIONS_PER_WALLET
+        }));
+        oar.submit{value: 0.001 ether}(OAR.SubmissionParams({
+            name: "name 2",
+            description: DESCRIPTION,
+            category: CATEGORY,
+            url: URL,
+            contractAddress: contractAddress,
+            functionSelector: FUNCTION_SELECTOR,
+            assetAddress: ASSET_ADDRESS,
+            assetAmount: ASSET_AMOUNT,
+            maxActionsPerWallet: MAX_ACTIONS_PER_WALLET
+        }));
+        oar.submit{value: 0.001 ether}(OAR.SubmissionParams({
+            name: "name 3",
+            description: DESCRIPTION,
+            category: CATEGORY,
+            url: URL,
+            contractAddress: contractAddress,
+            functionSelector: FUNCTION_SELECTOR,
+            assetAddress: ASSET_ADDRESS,
+            assetAmount: ASSET_AMOUNT,
+            maxActionsPerWallet: MAX_ACTIONS_PER_WALLET
+        }));
+
+        // When I sweep the OAR balance
+        vm.prank(ME);
         oar.sweep();
 
         // Then I should see the OAR balance in the wallet
-        //
+        assertEq(ME.balance, balance + 0.003 ether, "balance mismatch");
     }
 
     // Sad Paths
 
-    function testRevert_sweep_whenNotOwner() public {}
+    function testRevert_sweep_whenNotOwner() public {
+        // Then the function should revert
+        vm.expectRevert("UNAUTHORIZED");
+
+        // When I sweep the OAR balance as a non-owner
+        oar.sweep();
+    }
 }
